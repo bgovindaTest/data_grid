62fbe7ffeec3edb85a2e66948c331593b5eaba77
Brandon Govindarajoo <bgovi@umich.edu>
2022-07-14 15:28:48 -0400

    Placeholder added for CustomCellParams/index.js

--- src/lib/GridParser/ColumnDefs/CustomCellParams/index.js
+++ src/lib/GridParser/ColumnDefs/CustomCellParams/index.js
@@ -8,6 +8,14 @@ valuesObject: the json array object for autocomplete and richselector.
 
 //import crud column
 const crudColumn = require('./CrudColumn')
+const data_config = require('../../../DataConfig')
+const DateTimeParams = require('./date_time')
+// let cellEditors = {
+//     //customEditors have special formatting
+//     'customEditors':    ['autoComplete',  'subGridSelector', 'agRichSelectCellEditor'],
+//     'standardEditors' : ['agTextCellEditor', 'agLargeTextCellEditor', 'dateTimeSelector' ], //make popupTrue for agLargeTextCellEditor
+//     'defaultEditor': 'agTextCellEditor'
+// }
 
 class CustomCellParams {
     //for main loader
@@ -15,53 +23,40 @@ class CustomCellParams {
     //allowNull (prepends value?)
 
     //valuesArray created by grid_func mix in?
-    constructor(grid, valuesObject) {
+    constructor(grid, gridValuesObject) { //gridValuesObject = valuesObject[position_index]
         this.grid         = grid
         this.valuesObject = valuesObject
-        this.cellEditor   = ""
     }
     RunInit() {
 
         for(let i=0; i < this.grid.length; i++) {
-            this.cellEditor = this.grid_column['cellEditor'] || ""
-            if (this.cellEditor === "") { return }
-            this.AutoCompleteParams()
-            this.AgRichSelectParams()
-            this.DeleteUndoParams()
+            let cE = this.grid_column['cellEditor'] || "agTextCellEditor"
+            if (cE === "agTextCellEditor") { return }
+            else if ( cE  === "autoComplete" ) {
+                this.AutoCompleteParams()
+            } else if (cE === 'agLargeTextCellEditor') {
+                AgLargeTextParams()
+            } else if (cE === "agRichSelectCellEditor") {
+                AgRichSelectParams() 
+            } else if (cE === "dateTimeSelector") {
+                DateTimeParams()
+            } else if (cE === "subGridSelector") {
+                this.SubGridParams()
+            } else if (cE === "meta c") {
+                continue
+            }
+
 
 
         }
-        this.SubGridParams()
-        //crudColumn
-        //returns functions?
     }
 
-    AutoCompleteParams() {}
-    AgRichSelectParams() {}
-    DeleteUndoParams()   {}
-    SubGridParams()      {}
-    AgLargeTextParams() {}
+
 
 
 }
 
-// async CellEditorParams(grid_column) {
-//     /*
-//     Handles specialized modules. i.e. autocomplete and aggrid rich cell editor
 
-//     Adds valueGetter and valueSetter inorder to handle data processing.
-
-//     Pull Data if needed on init?
-
-//     */
-//     if (! grid_column.hasOwnProperty('cellEditor')) {return}
-//     let cedit = grid_column['cellEditor']
-//     //check cell editor type?
-//     if (cedit === 'agRichSelectCellEditor' ) {}
-//     else if (cedit === 'subGrid' ) {} //has placeholder for modal?
-//     else if (cedit === 'dateSelector' ) {}
-//     else if (cedit === 'autoComplete' ) {}
-// }
 
 
 

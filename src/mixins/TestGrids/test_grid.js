34bdd1551fcba516c0cd906127af541d8fc588af
Brandon Govindarajoo <bgovi@umich.edu>
2022-08-15 17:53:07 -0400

    changed pull from public to provider_effort in test_case

--- src/mixins/TestGrids/test_grid.js
+++ src/mixins/TestGrids/test_grid.js
@@ -32,7 +32,7 @@ let test_grid = {
 
         ],
         "routeParams": {
-            "default_route": "data/public/company"
+            "default_route": "data/provider_effort/company"
         },
         // "tableData": [
         //     {

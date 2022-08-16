998c805c5e38df5f7cb25c21315fb8ffe2da47ad
Brandon Govindarajoo <bgovi@umich.edu>
2022-08-16 09:41:44 -0400

    Added effort views completed prereqs. Need to convert to view and create current_effort view

--- src/mixins/TestGrids/test_grid.js
+++ src/mixins/TestGrids/test_grid.js
@@ -12,23 +12,9 @@ let test_grid = {
             },
             {"field": "company_name","editable": true, "showSort": true, "showFilter": true  },
             {"field": "company_code","editable": true, "showSort": true, "showFilter": true  },
-            {"field": "is_active","editable": true, "showSort": true, "showFilter": true  },
+            {"field": "is_active","editable": true, "showSort": true, "showFilter": true  }
             // {"field": "some_date","editable": true, "showSort": true, "showFilter": true, "dataType": "date",
             //     "cellEditor": 'dateTimeEditor', 'chmodParams': 'c'  },
-
-            {"field": "msg",  "editable": true, "showSort": true, 'cellEditor': "autoCompleteEditor",
-                "cloneOnCopy": false, "showFilter": true, 'chmodParams': 'rc',
-                'cellEditorParams': {
-                    "api_route": "/data/public/dropx"
-                    // "valuesObject": [
-                    //     {"msg": 'true', "id": "1"},
-                    //     {"msg": 'false',"id": "2"},
-                    //     {"msg": 'null',"id": "3"}
-                    // ]
-                }
-
-
-            }
 
         ],
         "routeParams": {

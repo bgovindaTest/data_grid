2a2535a70031bf020d11645136719aface20dc73
Brandon Govindarajoo <bgovi@umich.edu>
2022-08-18 15:10:35 -0400

    All pages implemented need to replace views with user level views

--- src/mixins/TestGrids/test_grid.js
+++ src/mixins/TestGrids/test_grid.js
@@ -3,4 +3,5 @@ const appointments = require('./appointments.js')
 const appointment_effort = require('./appointment_effort.js')
 const user_app_perms      = require('./user_app_perms.js')
 const user_org_permission = require('./user_specialty_perms.js')
-module.exports = user_org_permission
+const home_page           = require('./home_page.js')
+module.exports = appointment_effort //home_page

215e2ac604b9a279912e3df25e1f53763b225456
Brandon Govindarajoo <bgovi@umich.edu>
2022-08-23 10:24:36 -0400

    Added lob page

--- src/mixins/TestGrids/app_configs/org/lob.js
+++ src/mixins/TestGrids/app_configs/org/lob.js
@@ -1,12 +1,12 @@
-{
+const lob = {
     "comments": "This is the main landing page for the app. ProjectFolderName TableName (as link) Description",
     "grids": [
         {
             "columnDefs": [
-                { "headerName": "CompanyName",   "field": "company_name", "valueGetter":"lookup(company_id,   'lob_name')", "editable": false, "showSort": true, "showFilter": true},
-                { "headerName": "CompanyCode",   "field": "company_code", "valueGetter":"lookup(company_id,   'lob_code')", "editable": false, "showSort": true, "showFilter": true },
+                { "headerName": "CompanyName",   "field": "company_name", "valueGetter":"lookup(company_id,   'company_name')", "editable": false, "showSort": true, "showFilter": true},
+                { "headerName": "CompanyCode",   "field": "company_code", "valueGetter":"lookup(company_id,   'company_code')", "editable": false, "showSort": true, "showFilter": true },
                 { "headerName": "Companyid",     "field": "company_id",  "editable": true, "showSort": true, 
-                    "showFilter": true, "isRequired": true, "width": 380,
+                    "showFilter": true, "isRequired": true,
                     "cellEditor": "autoCompleteEditor",
                     "cellEditorParams": {
                         "api_route": "data/provider_effort/company_rv",
@@ -14,7 +14,7 @@
                                 {"field": "company_name"}, {"field": "company_code"},
                                 {"field": "id"} 
                             ],
-                        "displayKey": "lob_name"
+                        "displayKey": "id"
                     }
                 },
                 { "field": "is_active",   "dataType": "boolean", "editable": true, "showSort": true, "showFilter": true,
@@ -26,12 +26,17 @@
                 },
                 { "headerName":  "LobName","field": "lob_name", "editable": true, "showSort": true, "showFilter": true},                
                 { "headerName":  "LobCode","field": "lob_code", "editable": true, "showSort": true, "showFilter": true},
-                { "field": "id", "editable": false }
+                {"field": "last_modified_user_email", "chmodParams": "r", "editable": false, "showSort": true, "showFilter": true },
+                {"field": "updated_at", "chmodParams": "r", "editable": false, "showSort": true, "showFilter": true },
+
+                { "field": "id", "chmodParams": "rw", "editable": false, "showSort": true, "showFilter": true }
             ],
             "routeParams": {
-                "default": "/provider_effort/line_of_business",
+                "default_route": "/provider_effort/line_of_business",
                 "select": {"route":"data/provider_effort/line_of_business_rv/select"}
             }
         }
     ]
 }
+
+module.exports = lob
--- src/mixins/TestGrids/test_grid.js
+++ src/mixins/TestGrids/test_grid.js
@@ -7,10 +7,10 @@ const home_page           = require('./app_configs/landing_page.js')
 
 //org
 const company           = require('./app_configs/org/company.js')
-// const lob               = require('./app_configs/org/lob.js')
+const lob               = require('./app_configs/org/lob.js')
 // const department        = require('./app_configs/org/department.js')
 // const specialty         = require('./app_configs/org/specialty.js')
 // const cost_center       = require('./app_configs/org/cost_center.js')
 // const cost_center_time  = require('./app_configs/org/cost_center_time.js')
 
-module.exports =  company
+module.exports = lob // company

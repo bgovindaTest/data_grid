7aef35c5515d1746fbf110ce900bcd30d238d1b1
Brandon Govindarajoo <bgovi@umich.edu>
2022-08-25 13:02:35 -0400

    Added sub permissions for apps. Need to complete

--- src/mixins/TestGrids/jsToSqlJson.js
+++ src/mixins/TestGrids/jsToSqlJson.js
@@ -18,11 +18,20 @@ const fs = require('fs')
 let apps = [
     //main
     {'id': '1', 'project_name': 'provider_effort', 'table_name': 'home_page',
-        "page_config": JSON.stringify(home_page),  'is_public': 'true',  'description': "" },
+        "page_config": JSON.stringify(home_page),  'is_public': 'true',  'description': "",
+        'permissions': []
+    },
     {'id': '2', 'project_name': 'provider_effort', 'table_name': 'providers', 
-        "page_config":JSON.stringify(providers),    'is_public': 'false', 'description': "" },
+        "page_config":JSON.stringify(providers),    'is_public': 'false', 'description': "",
+        'permissions': ["provider_effort.providers.modify", "provider_effort.providers_lv.read_only", "provider_effort.providers_rv.read_only"]
+    },
     {'id': '3', 'project_name': 'provider_effort', 'table_name': 'appointments', 
-        "page_config":JSON.stringify(appointments), 'is_public': 'false', 'description': "" },
+        "page_config":JSON.stringify(appointments), 'is_public': 'false', 'description': "",
+        'permissions': [ "provider_effort.appointments_uv.modify", "provider_effort.appointments_rv.read_only",
+            "provider_effort.lcg_lv.read_only", "provider_effort.cpsc_lv.read_only",
+            "provider_effort.cost_centers_byuser_lv.read_only"
+        ]
+    },
     {'id': '4', 'project_name': 'provider_effort', 'table_name': 'appointment_effort',
         "page_config":JSON.stringify(appointment_effort), 'is_public': 'false', 'description': "" },
     //org heirarchy
@@ -58,3 +67,69 @@ let out_path = '/home/bgovi/PsqlCred/output_data/sql_admin/apps.psql'
 
 let out_str = insertStr + values.join(',\n') + ';'
 console.log(out_str)
+
+//schema.table_read_only
+//schema.table_modify
+/*
+INSERT INTO app_admin.registered_tables (permission_name, schema_name, table_name,
+    allow_select,allow_exists, allow_insert, allow_update, allow_delete, allow_save)
+
+
+    select schemaname|| '.'|| viewname || ".read_only" as permission_name, schemaname, viewname as tablename, true as allow_select, true as allow_exists,
+        false as allow_insert, false as allow_update , false as allow_delete, false as allow_save
+    from pg_catalog.pg_views
+    where schemaname IN ('provider_effort', 'app_admin')
+    UNION ALL
+    select schemaname|| '.'|| tablename || ".read_only" as permission_name, schemaname, viewname as tablename, true as allow_select, true as allow_exists,
+        false as allow_insert, false as allow_update , false as allow_delete, false as allow_save
+    from pg_catalog.pg_tables
+    where schemaname IN ('provider_effort', 'app_admin')
+
+    UNION ALL
+    select schemaname|| '.'|| tablename || ".modify" as permission_name, schemaname, tablename, true as allow_select, true as allow_exists,
+        true as allow_insert, true as allow_update , true as allow_delete, true as allow_save
+    from ( VALUES ( 'provider_effort','appointment_effort_byuser_uv'), ('provider_effort' , 'appointments_byuser_uv' ) ) as x (schemaname, tablename)
+    
+    UNION ALL
+    select schemaname|| '.'|| tablename || ".modify" as permission_name, schemaname, viewname as tablename, true as allow_select, true as allow_exists,
+        true as allow_insert, true as allow_update , true as allow_delete, true as allow_save
+    from pg_catalog.pg_tables
+    where schemaname IN ('provider_effort', 'app_admin')
+
+*/
+
+/*
+INSERT INTO app_admin.app_permissions (app_id , registered_table_id)
+SELECT id as registered_table_id
+FROM app_admin.registered_tables 
+CROSS JOIN (SELECT ${app_id} as app_id) x
+WHERE permission_name IN (${permissions});
+
+
+*/
+
+
+// CREATE TABLE app_admin.app_permissions (
+//     id bigserial PRIMARY KEY,
+//     app_id bigint REFERENCES app_admin.apps (id) NOT NULL,
+//     registered_table_id bigint REFERENCES app_admin.registered_tables (id) NOT NULL,
+//     created_at timestamptz default now(),
+//     updated_at timestamptz default now(),
+//     last_modified_userid   bigint  DEFAULT app_userid(),
+
+//     UNIQUE(app_id, registered_table_id)
+// );
+
+// COMMENT ON TABLE app_admin.app_permissions IS $$ 
+// permissions that are allowed by the apps configuration. This allows a registered table
+// to give different permissions for different apps.
+// $$;
+
+
+/*
+--user perms
+INSERT INTO app_admin.user_app_permissions(user_id, app_id, is_read_only)
+SELECT user_id, 2, false FROM app_admin.users
+UNION
+SELECT user_id, 3, false FROM app_admin.users;
+*/
